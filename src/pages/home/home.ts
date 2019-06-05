78ffe56dded7ecb0e81af690c81280e19f0586eb
bc-nogueira <bcnogueira@id.uff.br>
2019-06-05 00:00:22 -0300

    Initial commit.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  disabled = false;
+  showMessage = false;
+  showButton = true;
+  stateTxt = '';
+  showSpinner = false;
+  tamanho = 1000; // Change for testings
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) { }
 
+  start() {
+    this.disabled = true;
+    this.showButton = false;
+    this.showSpinner = true;
+    this.stateTxt = 'Started';
+
+    let id = setInterval(() => {
+      // Change size of loop for testing
+      for(let i = 0; i < 1; i++) {
+        let array =  Array.from({length: this.tamanho}, () => 
+          1
+        );
+        array = null;
+      }
+      this.disabled = false;
+      this.showMessage = true;
+      this.showSpinner = false;
+      this.stateTxt = 'Finished';
+    }, 100);
   }
-
 }
